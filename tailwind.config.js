810d30e985bca1e83f0fe7da5e5be8fcd07a662e
Arif Rizki <arifwise67@gmail.com>
2024-05-28 21:56:51 +0800

    draft 1 web catalog foton graphia

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
