d8216808455022e935f0c7663e9f3ec6ff0e1abf
Arif Rizki <arifwise67@gmail.com>
2024-05-30 22:43:24 +0800

    memperbaiki fitur hamburger menu pada versi mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}"],
+  content: ["./public/**/*.{html,js}", "index.html"],
   theme: {
     extend: {
       fontFamily: {
