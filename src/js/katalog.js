df3705b174f1a2cce7cdede035ab70a3e2a1d47c
Arif Rizki <arifwise67@gmail.com>
2024-06-09 22:11:09 +0800

    mengganti alpine ke vanilla js, dan memperbaiki fitur faq

---
+++ src/js/katalog.js
@@ -0,0 +1,37 @@
+export const slides = [
+  {
+    id: 1,
+    name: "Stiker Bontak",
+    categori: "Cetak A3+",
+    desc: "Cetak stiker mulai harga Rp. 5.000,-",
+    img: "src/img/slides/bontak-slide.png",
+  },
+  {
+    id: 2,
+    name: "Sablon Baju/Kaos",
+    categori: "Sablon DTF",
+    desc: "Sablon kaos sesuai keinginan dan selera anda",
+    img: "src/img/slides/baju-slide.png",
+  },
+  {
+    id: 3,
+    name: "Kalender Duduk",
+    categori: "Cetak Kalender",
+    desc: "Bikin kalender untuk menghias meja kamu",
+    img: "src/img/slides/kalender-slide.png",
+  },
+  {
+    id: 4,
+    name: "X/Y Banner",
+    categori: "Banner Promosi",
+    desc: "Cetak X Banner untuk usaha atau event kamu",
+    img: "src/img/slides/x-bannerr-slide.png",
+  },
+  {
+    id: 5,
+    name: "Jam Dinding",
+    categori: "Souvenir & Hadiah",
+    desc: "jam custom untuk hadiah atau hiasan kamarmu",
+    img: "src/img/slides/jam-dinding-slide.png",
+  },
+];
